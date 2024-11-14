a3c8ca23e384e3fd83db57838a73f333227a985e
OsmarVillegas <osmarvm25@gmail.com>
2024-11-14 04:03:46 -0600

    Initial commit

---
+++ Assets/Scripts/MonoBehaviours/Character.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character : MonoBehaviour
+{
+    public HitPoints hitPoints; //Puntos actuales de jugador
+    public float maxHitPoints; //Máximos puntos a obtener
+}
